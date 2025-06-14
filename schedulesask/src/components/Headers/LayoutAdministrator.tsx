80efd5e6456d48bdc88c7f22169f6b4dc17e2de4
Ajmuha8988 <Borlakovmuhammad@mail.ru>
2025-06-14 20:12:28 +0400

    Исправление бага часть 4

--- schedulesask/src/components/Headers/LayoutAdministrator.tsx
+++ schedulesask/src/components/Headers/LayoutAdministrator.tsx
@@ -24,7 +24,7 @@ const LayoutAdministrator = () => {
                     <header className="bg-dark font-for-headers">
                         <nav className="navbar navbar-dark navbar-expand-lg navbar-warning bg-dark container">
                             <a className="navbar-brand" onClick={() => navigate('/administrator')} >
-                                <img width="150" height="50" src="https://drive.google.com/file/d/1WAEqMlW7XGP4vbLK6O0AYZIY8q9r25Le/view?usp=sharing" alt="Alternate Text" />
+                                <img width="150" height="50" src="https://disk.yandex.ru/i/dwzppAAGNRRSLQ" alt="Alternate Text" />
                             </a>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon "></span>
